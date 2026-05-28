1367d2fb499bc2b2a518c51231da02f7e2d6aeb1
ruship13 <rushikeshpathare71@gmail.com>
2026-05-28 10:26:53 +0530

    for pipeline changes

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
 
-        model.addAttribute("message", "Welcome To Rushi Word!");
+        model.addAttribute("message", "Hello from Spring Boot!");
 
         return "index";
     }
